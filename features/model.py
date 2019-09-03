54db90927ea994252f1e567b74b07f92bdcfa0c8
Matthew Fielding <mattfielding8@gmail.com>
2019-09-03 18:12:20 +0100

    Feat: add sigmoid function
    Used to calculate the sigmoid of X and theta.

--- features/model.py
+++ features/model.py
@@ -2,3 +2,10 @@
 regression model"""
 
 import numpy as np
+
+def sigmoid(X, theta):
+    '''
+    Returns the sigmoid of  theta.T.X
+    Precondition: if X is (m x n), theta should be (n x i)
+    '''
+    return 1/(1+np.exp(-np.dot(X,theta)))
