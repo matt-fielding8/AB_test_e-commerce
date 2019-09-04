a824563d9625a831dd34dd8e3c3763cc4f5ffc56
Matthew Fielding <mattfielding8@gmail.com>
2019-09-04 12:34:29 +0100

    Feat: adds gradientDescent function
    Runs gradient descent on X, y, theta. Returns optimised theta and costs
    if track==True for optimising alpha.

--- features/model.py
+++ features/model.py
@@ -18,3 +18,21 @@ def computeCost(X, theta, y):
     h = sigmoid(X, theta)
     cost = (1/m)*np.sum(-y*np.log(h)-(1-y)*np.log(1-h))
     return cost
+
+def gradientDescent(X, theta, y, alpha=0.1, iters=1000, track=False):
+    '''
+    Optimises theta via gradient descent. If track==True, the cost is computed
+    50 times at intervals to track convergence.
+    '''
+    m = X.shape[0]
+    costs = []
+    for i in range(iters+1):
+        h = sigmoid(X, theta)
+        gradients = (1/m)*np.dot(X.T,(h-y))
+        theta = theta - alpha*gradients
+        # Compute costs
+        if track and ((i) % (iters/50) == 0):
+            costs.append(computeCost(X, theta, y))
+    if track:
+        return theta, costs
+    return theta
